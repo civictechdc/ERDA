5ebd7b79cac5ac5e5ae0face13b95435ec036446
Scott Bai <scott.bai@gmail.com>
2014-05-03 16:36:00 -0400

    Add histogram of response times.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-console.log('This would be the main JS file.');
+function responseTimes(hist) {
+  var apiURL = 'http://54.187.110.72/emr_data/?page=0&per_page=100';
+  d3.json(apiURL, function(error, events) {
+    if (error) return console.warn(error);
+    var responseTimes = events.map(function(d) {
+      return moment.duration(d.response_time).asSeconds();
+    });
+    hist.data(responseTimes)
+      .render();
+  });
+}
+
+function go() {
+  // create the histogram chart object
+  var hist = new histogram().parent(d3.select('#histogram'));
+  responseTimes(hist);
+}
