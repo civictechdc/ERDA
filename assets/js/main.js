11a959f13e8d35979cf1347e52b5c01f78342eda
Scott Bai <scott.bai@gmail.com>
2014-05-16 20:30:08 -0400

    Fix more paths.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
+---
+---
 function go() {
   // create the histogram chart object
   var hist = new histogram().parent(d3.select('#histogram'));
-  var dataURL = '/data/responseTimeDistribution.json';
+  var dataURL = '{{site.baseurl}}/data/responseTimeDistribution.json';
   d3.json(dataURL, function(error, data) {
     if (error) return console.warn(error);
     var bins = data.map(function(count, i) {
