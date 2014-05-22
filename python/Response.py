65455a494573f012152da45084274c37027b0898
alexlyte <alexanderlyte@gmail.com>
2014-05-21 22:54:31 -0400

    Fixed some python errors

--- python/Response.py
+++ python/Response.py
@@ -4,11 +4,11 @@
 from os import listdir
 from os.path import isfile, join	
 import csv
-import pandas as pd
 import glob
+import pandas as pd
 
 # Takes in a string time value in HH:MM:SS format, outputting an integer seconds
-def timeToSeconds(strDate):
+def stringToSeconds(strDate):
 	secs = 0
 	if strDate:
 		time = strDate.split(":")
@@ -39,6 +39,9 @@ def stringToQuadrant(strQuadrant):
 	else:
 		return "NA"
 
+
+
+
 if __name__ == "__main__":
 
 	mypath = raw_input('What is the path to the ERDA data? \n ')
@@ -48,55 +51,58 @@ if __name__ == "__main__":
 
 	print(erdafiles)
 
-	e1 = open(erdafiles[0], "r")
-	e2 = open(erdafiles[1], "r")
-	allFiles = glob.glob(path + "/*.csv")
-    frame = pd.DataFrame()
-    list = []
-    for files in allFiles:
-        df = pd.read_csv(join(directory,files),index_col=None, header=0)
-        list.append(df)
-    frame = pd.concat(list)
+	e1 = open(mypath + "/" + erdafiles[0], "r")
+	e2 = open(mypath + "/" + erdafiles[1], "r")
+	allFiles = glob.glob(mypath + "/*.csv")
+frame = pd.DataFrame()
+list = []
+for files in allFiles:
+    df = pd.read_csv(join(mypath,files),index_col=None, header=0)
+    list.append(df)
+frame = pd.concat(list)
 
 	# Open and read file
 	#	data = open('fullEmergencyData.csv', "r")
 	#	emer = csv.DictReader(data)
 
-	# Final Columns
-	response_time = []
-	vehicle_type = []
-	city_quadrant = []
-	time_of_day = []
-	month = []
-	holiday = []
+# Final Columns
+response_time = []
+vehicle_type = []
+city_quadrant = []
+time_of_day = []
+month = []
+holiday = []
 
-	# Response Time
-	for row in emer:
-		time = row['Response.Time..HH.MM.SS.']
+emer = frame
+#print(frame['Response.Time..HH.MM.SS.', ])
+# Response Time
+for row in emer:
+	print(row)
+	time = row['Response.Time..HH.MM.SS.']
+	
+	response_time.append(stringToSeconds(time))
+	
+# Vehicle Type
+	vehicle = row['Unit']
+	vehicle_type.append(translateVehicleType(vehicle))
 		
-		response_time.append(stringToSeconds(time))
-		
-	# Vehicle Type
-		vehicle = row['Unit']
-		vehicle_type.append(translateVehicleType(vehicle))
-			
-	# City Quadrant
-		address = row['Location']
-		city_quadrant.append(stringToQuadrant(address))
-		
-	# Time of Day
-		hr = int((row['Dispatch.Time..HH.MM.SS.'].split(":"))[0])
-		time_of_day.append(hr)
+# City Quadrant
+	address = row['Location']
+	city_quadrant.append(stringToQuadrant(address))
+	
+# Time of Day
+	hr = int((row['Dispatch.Time..HH.MM.SS.'].split(":"))[0])
+	time_of_day.append(hr)
 
-	# Month
-		mon = int((row['Date'].split("/"))[0])
-		month.append(mon)
+# Month
+	mon = int((row['Date'].split("/"))[0])
+	month.append(mon)
 
-	# Write to CSV
-	rows = zip(response_time, vehicle_type, city_quadrant, time_of_day, month)
+# Write to CSV
+rows = zip(response_time, vehicle_type, city_quadrant, time_of_day, month)
 
-	with open('test.csv', 'w') as f:
-		writer = csv.writer(f)
-		for row in rows:
-			writer.writerow(row)
+with open('test.csv', 'w') as f:
+	writer = csv.writer(f)
+	for row in rows:
+		writer.writerow(row)
 
