185ea37773057065770856ea08412a48bc96db56
Scott Bai <sbai@mitre.org>
2014-05-27 15:38:41 -0400

    Server API changes: - Import from CSV instead of sqlite. - Use Python 3.4.0. - Fix #4. - Simplify directory structure. - Make server executable. - Fix indendation errors. - Add missing flask-cors to requirements.txt. - Remove wsgi_ref from requirements.txt for Python 3.

--- api/api.py
+++ api/api.py
@@ -1,4 +1,10 @@
-from flask import Flask, jsonify, request, make_response
+#! /usr/bin/env python
+# encoding: utf-8
+
+'''
+ERDA data service API.
+'''
+from flask import Flask, jsonify, request, make_response, Response
 import json
 from pymongo import MongoClient
 from bson import json_util
@@ -6,14 +12,14 @@ from bson import json_util
 from werkzeug.contrib.fixers import ProxyFix
 
 con  = MongoClient()
-db = con.emr_db
+db = con.erda
 
 try:
-    # this is how you would normally import
-    from flask.ext.cors import cross_origin
+	# this is how you would normally import
+	from flask.ext.cors import cross_origin
 except:
-    # support local usage without installed package
-    from flask_cors import cross_origin
+	# support local usage without installed package
+	from flask_cors import cross_origin
 
 app = Flask(__name__)
 
@@ -22,34 +28,29 @@ app = Flask(__name__)
 def not_found(error):
 	return make_response(jsonify({'error': 'Not found'}), 404)
 
-
 class InvalidUsage(Exception):
-    status_code = 400
+	status_code = 400
 
-    def __init__(self, message, status_code=None, payload=None):
-        Exception.__init__(self)
-        self.message = message
-        if status_code is not None:
-            self.status_code = status_code
-        self.payload = payload
+	def __init__(self, message, status_code=None, payload=None):
+		Exception.__init__(self)
+		self.message = message
+		if status_code is not None:
+			self.status_code = status_code
+		self.payload = payload
 
-    def to_dict(self):
-        rv = dict(self.payload or ())
-        rv['message'] = self.message
-        return rv
-
+	def to_dict(self):
+		rv = dict(self.payload or ())
+		rv['message'] = self.message
+		return rv
 
 @app.errorhandler(InvalidUsage)
 def handle_invalid_usage(error):
-    response = jsonify(error.to_dict())
-    response.status_code = error.status_code
-    return response
-
-
+	response = jsonify(error.to_dict())
+	response.status_code = error.status_code
+	return response
 
 @app.route("/emr_data/", methods=['GET'])
 @cross_origin()
-
 #full data set api iteraion 0.1
 def test():
 	#returns a list of emr responses - with page  and  per_page  arguments
@@ -63,7 +64,7 @@ def test():
 	# check if page is less then 1 or number of pages requested is 
 	# out of range  - if les then 1  set page to 0 if out of range set to 
 	# max range
-	emr_data = db.emr
+	emr_data = db.events
 	count = emr_data.find().count()
 	#set the maximum results per page at 1000
 	if results_per_page > 1000:
@@ -80,19 +81,17 @@ def test():
 	else:
 		off = results_per_page * (page - 1)
 
-	
 	x = [doc for doc in emr_data.find().skip(off).limit(lim)]
 	json_txt = json.dumps(x, default = json_util.default)
 	
-	return json_txt
-	
+	return Response(json_txt, mimetype='application/json')
 
 #histogram iteration 0.2
 @app.route("/histogram/", methods=['GET'])
 @cross_origin()
 def hist():
 	bucket = int(request.args.get('bucket', 60))
-	emr_data = db.emr
+	emr_data = db.events
 	emr_data.aggregate([{ '$project' :
 			{'bucket' : {'$divide' :[{'$subtract' : ['$response_seconds',
 						 {'$mod' : ['$response_seconds', bucket]}]}, bucket]
@@ -105,38 +104,33 @@ def hist():
 				}
 			},
 		{'$out' : 'hist_out'}
-		])	
-	
+		])
 
-	
 	x = [doc for doc in db.hist_out.find()]
-        json_txt = json.dumps(x, default = json_util.default)
+	json_txt = json.dumps(x, default = json_util.default)
 	
-	return json_txt	
-
+	return Response(json_txt, mimetype='application/json')
 
 @app.route("/emr_data_by_field/", methods=['GET'])
 def by_field():
-	emr_data = db.emr	
+	emr_data = db.events
 	y = emr_data.find_one()
 	keys = y.keys()
 	fields = request.values.getlist('field')
 	field_list = []
 	if len(fields) > 1:
 		raise InvalidUsage('You can only select 1 feature at a time', status_code = 400)
-		
+	
 	for f in keys:
 		if f  in fields:
 			field_list.append(f)
-		
+	
 	x = [doc[field_list[0]] for doc in emr_data.find(fields = field_list)]
-			
+	
 	json_txt = json.dumps(x, default = json_util.default)
-	return json_txt
-
+	return Response(json_txt, mimetype='application/json')
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
-	
 
 if __name__ == '__main__':
 	app.run()
---
+++ api/import.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+'''
+Imports a CSV file, containing DC emergency response data, to MongoDB.
+'''
+import sys
+import csv
+import time
+from pymongo import MongoClient
+
+def import_erda_csv(file_name):
+	print(file_name)
+	start = time.time()
+	data = []
+	with open(file_name, 'r') as csv_file:
+		reader = csv.reader(csv_file)
+		for row in reader:
+			data.append(row)
+		elapsed = '{:2.2f}'.format(time.time() - start)
+		print('Read', len(data) - 1, 'records in', elapsed, 's')
+	return data
+
+def init_db(data):
+	print('Creating database tables...')
+	start = time.time()
+	mongo_con = MongoClient()
+	erda_db = mongo_con.erda
+	events_collection = erda_db.events
+	events = iter(data)
+	header = next(events)
+	for event in events:
+		events_collection.insert({
+			header[0] : event[0], header[1] : event[1], header[2] : event[2],
+			header[3] : event[3], header[4] : event[4], header[5] : event[5]
+		})
+	elapsed = '{:2.2f}'.format(time.time() - start)
+	print('Imported', len(data) - 1, 'records in', elapsed, 's')
+
+def usage():
+	'''Prints a help string.'''
+	print("usage: import.py <filename>")
+
+if __name__ == "__main__":
+	if (len(sys.argv) == 2):
+		init_db(import_erda_csv(sys.argv[1]))
+	else:
+		usage()
