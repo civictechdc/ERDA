5a9f2f86f0729bf724daa5b785075e1a8e98da86
Gabriel Marcano <gabemarcano@yahoo.com>
2014-07-22 20:30:49 -0400

    Added functionality to be able to provide multiple sheet names and multiple xlsx files simultaneously. When multiple files are provided, the script tries to match the listed sheets in all files. Any sheets not found print a message, and the script continues.

--- python/XlsxToCsv.py
+++ python/XlsxToCsv.py
@@ -3,31 +3,35 @@
 import openpyxl
 import csv
 import argparse
+import sys
 
 def writeCSV(filename, sheet, sheetname):
     if sheet:
-        csvfile = filename
+        lst = filename.split('.')
+        lst.pop()
+        csvfile = ''.join(lst)
         csvfile += '-Sheet-' + sheetname + '.csv'
         with open(csvfile, 'w', newline='') as f:
             c = csv.writer(f)
             for row in sheet.iter_rows():
                 c.writerow([cell.value for cell in row])
+    else:
+        print(filename + ": " + sheetname + " sheet could not be found! Continuing...", file=sys.stderr)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Convert XLSX files to CSV.')
-    parser.add_argument('--sheet', '-s', dest='sheet' , type=str, help='Sheet name in XLSX to export.')
-    parser.add_argument('File', type=str, help='XLSX to export.')
+    parser = argparse.ArgumentParser(description='Convert XLSX files to CSV. Use \'--\' to separate sheet names and XLSX file names.')
+    parser.add_argument('-s', '--sheets', dest='sheets',nargs="*", type=str, help='Sheet name in XLSX to export.')
+    parser.add_argument('Files', type=str, nargs="+" ,help='XLSX files to export.')
     args = parser.parse_args()
-    
-    wb = openpyxl.load_workbook(filename = args.File, use_iterators = True)
-    lst = args.File.split('.')
-    lst.pop()
-    csvfile = ''.join(lst)
-    
-    if (args.sheet):
-        sheet = wb.get_sheet_by_name(name = args.sheet)
-        writeCSV(csvfile, sheet, args.sheet)
-    else:
-        for sheetname in wb.get_sheet_names():
-            sheet = wb.get_sheet_by_name(name = sheetname)
-            writeCSV(csvfile, sheet, sheetname)
+    for xlsxFilename in args.Files:
+        wb = openpyxl.load_workbook(filename = xlsxFilename, use_iterators = True)
+
+        csvfile = xlsxFilename
+        if (args.sheets):
+            for sheetname in args.sheets:
+                sheet = wb.get_sheet_by_name(name = sheetname)
+                writeCSV(csvfile, sheet, sheetname)
+        else:
+            for sheetname in wb.get_sheet_names():
+                sheet = wb.get_sheet_by_name(name = sheetname)
+                writeCSV(csvfile, sheet, sheetname)
